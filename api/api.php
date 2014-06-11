1a18a3b69fb55f356eaf108cbe412ac35064e3ed
Matthieu Guffroy <mattgu74@gmail.com>
2014-06-11 15:21:41 +0200

    debut d'archi de l'api

--- api/api.php
+++ api/api.php
@@ -5,5 +5,21 @@ require_once '../config.inc.php';
 
 if(!isset($_REQUEST['action'])) {
     echo json_encode(array("error" => "action is mandatory"));
+    die();
+} else {
+    $action = $_REQUEST['action'];
+}
+
+function test() {
+    echo json_encode(array("success" => "Test is ok"));
+}
+
+switch($action) {
+   case 'test':
+       test();
+       break;
+   default:
+       echo json_encode(array("error" => "action $action is not recognized"));
+       break;
 }
 
